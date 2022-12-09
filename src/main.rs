78bc448f818f9413a9ffd8ffce2d42046e0112f5
Eric Hagman <eric@instawork.com>
2022-12-09 11:37:22 -0500

    Updated code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
 struct Board {
-    cells: [[i32; 10]; 10],
+    cells: [[Cell; 10]; 10],
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum Cell {
+    Empty,
+    PartialShip(Player),
+    Hit,
+    Miss,
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum Player {
+    Human,
+    Computer,
 }
 
 impl Board {
     fn print(&self){
         for x in self.cells {
             for y in x {
-                print!("{}", y);
+                print!("{:?} ", y);
             }
             println!();
         }
     }
     fn make_guess(&mut self, row: usize, col: usize) {
-        if self.cells[row][col] == 1 {
+        if self.cells[row][col] == Cell::PartialShip(Player::Computer) {
             // its a hit
-            self.cells[row][col] = 2;
+            self.cells[row][col] = Cell::Hit;
+        }
+        else {
+            self.cells[row][col] = Cell::Miss;
         }
     }
     fn has_winner(&self) -> bool {
         let mut count = 0;
         for x in &self.cells {
             for y in x {
-                if *y == 2 {
+                if *y == Cell::Hit {
                     count += 1;
                 }
             }
@@ -38,14 +55,20 @@ fn main() {
     // 4. Mark hit or miss
     // 5. Check winner
     // 6. Repeat 3 - 5
-    let mut board = Board{ cells: [[0; 10]; 10] };
+    let mut board = Board{ cells: [[Cell::Empty; 10]; 10] };
     board.print();
-    board.cells[0][0] = 1;
-    board.cells[0][1] = 1;
-    board.make_guess(0, 0);
-    println!("{}", board.has_winner());
-    board.print();
-    board.make_guess(0, 1);
-    println!("{}", board.has_winner());
+    board.cells[0][0] = Cell::PartialShip(Player::Computer);
+    board.cells[0][1] = Cell::PartialShip(Player::Computer);
+    loop {
+        let mut guess = String::new(); 
+        std::io::stdin().read_line(&mut guess).unwrap();
+        let guess_list: Vec<usize> = guess.trim().split(",").map(|f|f.parse().unwrap()).collect();
+        board.make_guess(guess_list[0], guess_list[1]);
+        board.print();
+        if board.has_winner() {
+            println!("Winner! {}", board.has_winner());
+            break;
+        }
+    }
     board.print();
 }
